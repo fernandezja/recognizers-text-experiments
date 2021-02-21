68f06083a339698ad2260d2a26ea6e43b660f5f7
fernandezja <fernandezja@gmail.com>
2021-02-21 09:39:50 -0300

    Recognize Percentage (simple phrases)

--- tests/RecognizersTextConsoleAppTests/RecognizeNumberTest.cs
+++ tests/RecognizersTextConsoleAppTests/RecognizeNumberTest.cs
@@ -14,13 +14,13 @@ namespace RecognizersTextConsoleAppTest
         [InlineData("Mis cincuenta centavos", "50", "integer")]
         [InlineData("Ni dos pesos", "2", "integer")]
         [InlineData("Mas vale pajaro en mano que cien volando", "100", "integer")]
-        public void RecognizeNumberSimple(string phrase, string numberExpected, string subtypeExpected)
+        public void RecognizeNumberSimple(string phrase, string valueExpected, string subtypeExpected)
         {
             var _sut = new RecognizeNumberSpanish();
             var result = _sut.RecognizeNumber(phrase);
 
             Assert.Equal(subtypeExpected, result["subtype"]);
-            Assert.Equal(numberExpected, result["value"]);
+            Assert.Equal(valueExpected, result["value"]);
 
         }
 
@@ -38,14 +38,14 @@ namespace RecognizersTextConsoleAppTest
         [InlineData("centesimo sexta", "106")]
         [InlineData("milesimo", "1000")]
         [InlineData("millonesimo", "1000000")]
-        public void RecognizeOrdinalSimple(string phrase, string numberExpected)
+        public void RecognizeOrdinalSimple(string phrase, string valueExpected)
         {
             var _sut = new RecognizeNumberSpanish();
             var result = _sut.RecognizeOrdinal(phrase);
 
-            Assert.Equal(numberExpected, result["offset"]);
+            Assert.Equal(valueExpected, result["offset"]);
             Assert.Equal("start", result["relativeTo"]);
-            Assert.Equal(numberExpected, result["value"]);
+            Assert.Equal(valueExpected, result["value"]);
 
         }
 
@@ -54,16 +54,31 @@ namespace RecognizersTextConsoleAppTest
         [InlineData("entre uno y dos", "[1,2)")]
         [InlineData("entre 10 y quince", "[10,15)")]
         [InlineData("entre veintidos y 33", "[22,33)")]
-        public void RecognizeNumberRangeSimple(string phrase, string numberExpected)
+        public void RecognizeNumberRangeSimple(string phrase, string valueExpected)
         {
             var _sut = new RecognizeNumberSpanish();
             var result = _sut.RecognizeNumberRange(phrase);
 
-            Assert.Equal(numberExpected, result["value"]);
+            Assert.Equal(valueExpected, result["value"]);
         }
 
 
-        
+
+        [Theory]
+        [InlineData("al llegar al cien por ciento", "100%")]
+        [InlineData("al llegar al 100%", "100%")]
+        [InlineData("debe tener mas del cincuenta por ciento", "50%")]
+        [InlineData("25%", "25%")]
+        [InlineData("mayor al 15 por ciento", "15%")]
+        [InlineData("menor al 44 por ciento", "44%")]
+        public void RecognizePercentageSimple(string phrase,string valueExpected)
+        {
+            var _sut = new RecognizeNumberSpanish();
+            var result = _sut.RecognizePercentage(phrase);
+
+            Assert.Equal(valueExpected, result["value"]);
+        }
+
 
     }
 }
